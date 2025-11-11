dcc377a7104e175266951dae0929af65d9c485f8
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-11-11 08:45:54 +0545

    Trionn Landing Page

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from '../Component/Navbar'
+import Hero from '../Component/Hero'
+import Buttons from '../Component/Buttons'
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        
+    </div>
+  )
+}
+
+export default LandingPage
