29210b87ed091fb8a70e0c1cdfba5dd637f1c945
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-11-11 20:34:40 +0545

    Landing page added

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import { Navbar } from '../Component/Navbar'
 import Hero from '../Component/Hero'
 import Buttons from '../Component/Buttons'
+import Scroll from '../Component/Scroll'
+
+import Recent from '../Component/Recent'
+import LoftRoom from '../Component/LoftRoom'
+import IMusic from '../Component/IMusic'
+
 const LandingPage = () => {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Scroll />
+        <Recent />
+        <LoftRoom />
+        <IMusic />
+
+
         
     </div>
   )
